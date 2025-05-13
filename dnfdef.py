71c09ec09765c95a0f5550562a8c0f393680f423
mogsor <mogsor@protonmail.com>
2025-05-13 22:45:35 +0100

    description adjustment

--- dnfdef.py
+++ dnfdef.py
@@ -4,7 +4,8 @@ the process:
 0. figure out which groups and packages to install, etc.
 
 1.  set every package as user-installed
-    (this seems insane, but it means we can remove groups without removing any packages, which allows us to manage groups independently)
+    (this seems insane, but it means we can remove groups without removing any
+    packages, which allows us to manage groups independently)
 
 2.  remove all groups
     (gives us a clean break)
@@ -17,25 +18,20 @@ the process:
     (fills in any newly-added packages)
 
 5.  set all packages to dependency, EXCLUDING explicitly desired packages
-    (in dnf5, if a package is part of a group and you mark it as a dependency, it ends up marked as a group package and won't be removed on autoremove. while this is kind of annoying on paper it's quite useful in practice, as it means we can simply set every package as dependency and let dnf figure out which ones to keep grouped)
+    (in dnf5, if a package is part of a group and you mark it as a dependency,
+    it ends up marked as a group package and won't be removed on autoremove.
+    while this is kind of annoying on paper it's quite useful in practice, as
+    it means we can simply set every package as dependency and let dnf figure
+    out which ones to keep grouped)
     (and then we exclude explicitly desired packages so they stay user marked)
 
 6.  dnf autoremove
     (finally clears out anything undesireable)
 
-an obvious problem with this approach is that it uses several dnf actions, which will spam the history a bit. there's almost certainly a better approach to this, but I'm not interested in learning the dnf5 api to find it, and history rollback is still *usable* after all
-"""
-
-
-"""
-simpler setup?
-
-1. set every package as a dependency
-2. install desired groups
-3. remove undesired groups
-4. install desired packages
-5. set desired packages as user installed
-6. dnf autoremove
+an obvious problem with this approach is that it uses several dnf actions,
+which will spam the history a bit. there's almost certainly a better approach
+to this, but I'm not interested in learning the dnf5 api to find it, and
+history rollback is still *usable* after all
 """
 
 
